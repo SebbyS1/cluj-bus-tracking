c8de49d42703a2391df3e96838b9a0cb651a127b
SebbyS1 <iove.sebastian@yahoo.com>
2026-03-28 20:21:49 +0200

    Mobile PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'cluj-bus-v1';
+const ASSETS = [
+  './cluj-bus-tracker.html',
+  './manifest.json',
+  './icon-192.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
